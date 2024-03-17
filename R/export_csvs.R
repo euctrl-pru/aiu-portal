944a49099e9384a3690f1c459af719bfa1b10a7d
SPINIELLI Enrico <enrico.spinielli@eurocontrol.int>
2024-03-17 12:07:55 +0100

    fix missing YEAR column and sorting

--- R/export_csvs.R
+++ R/export_csvs.R
@@ -6,7 +6,6 @@
 
 library(aiuportal)
 library(tidyverse)
-# library(here)
 library(fs)
 library(readxl)
 
@@ -14,60 +13,71 @@ library(readxl)
 dest_dir_root <- "C:\\Users\\spi\\EUROCONTROL\\ECTL - Aviation Intelligence Unit - AIU Portal\\Download data files"
 dest_folder <- "csv"
 
-export_co2_emissions(wef = "2000-01-01") -> cc
+export_co2_emissions(wef = "2024-01-01") -> cc
 cc |>
+  arrange(YEAR, MONTH, STATE_NAME) |>
   group_by(YEAR) |> 
   group_walk(~ write_csv(
     .x, 
     fs::path(dest_dir_root,
              dest_folder,
              stringr::str_glue("co2_emmissions_by_state_{YYYY}.csv", YYYY = .y$YEAR)),
-    na = ""))
+    na = ""),
+    .keep = TRUE)
 
-export_vertical_flight_efficiency(wef = "2000-01-01") -> vv
+export_vertical_flight_efficiency(wef = "2024-01-01") -> vv
 vv |>
+  arrange(YEAR, MONTH_NUM, APT_ICAO) |>
   group_by(YEAR) |> 
   group_walk(~ write_csv(
     .x, 
     fs::path(dest_dir_root,
              dest_folder,
              stringr::str_glue("vertical_flight_efficiency_{YYYY}.csv", YYYY = .y$YEAR)),
-    na = ""))
+    na = ""),
+    .keep = TRUE)
 
 
 # DB contains only last 2 years...so spit from Excel first...
-excel <- "ATC_Pre-Departure_Delay.xlsx"
-aa <- read_xlsx(fs::path(dest_dir_root, excel), sheet = "DATA")
-aa |>
-  dplyr::select(-`Pivot Label`) |>
-  dplyr::mutate(FLT_DATE = lubridate::as_date(.data$FLT_DATE, tz = "UTC")) |>
-  group_by(YEAR) |> 
-  group_walk(~ write_csv(
-    .x, 
-    fs::path(dest_dir_root,
-             dest_folder ,
-             stringr::str_glue("atc_pre_departure_delays_{YYYY}.csv", YYYY = .y$YEAR)), 
-    na = ""))
+# excel <- "ATC_Pre-Departure_Delay.xlsx"
+# aa <- read_xlsx(fs::path(dest_dir_root, excel), sheet = "DATA")
+# aa |>
+#   dplyr::select(-`Pivot Label`) |>
+#   dplyr::mutate(FLT_DATE = lubridate::as_date(.data$FLT_DATE, tz = "UTC")) |>
+#   arrange(YEAR, MONTH_NUM, FLT_DATE, APT_ICAO) |>
+#   group_by(YEAR) |>
+#   group_walk(~ write_csv(
+#     .x,
+#     fs::path(dest_dir_root,
+#              dest_folder ,
+#              stringr::str_glue("atc_pre_departure_delays_{YYYY}.csv", YYYY = .y$YEAR)),
+#     na = ""),
+#     .keep = TRUE)
 
 # ... and then overwrite with what comes from DB
-export_atc_predeparture_delay(wef = "2000-01-01") -> aa
+export_atc_predeparture_delay(wef = "2024-01-01") -> aa
 aa |>
+  arrange(YEAR, MONTH_NUM, FLT_DATE, APT_ICAO) |>
   group_by(YEAR) |> 
   group_walk(~ write_csv(
     .x, 
     fs::path(dest_dir_root,
              dest_folder ,
              stringr::str_glue("atc_pre_departure_delays_{YYYY}.csv", YYYY = .y$YEAR)),
-    na = ""))
+    na = ""),
+    .keep = TRUE)
 
 # we have data from previous years, BUT Excel starts at 2016
-export_atfm_slot_adherence(wef = "2016-01-01") -> ss
+# Export initially from 2016, then from 2024 onward
+export_atfm_slot_adherence(wef = "2024-01-01") -> ss
 ss |>
+  arrange(YEAR, MONTH_NUM, FLT_DATE, APT_ICAO) |>
   group_by(YEAR) |> 
   group_walk(~ write_csv(
     .x, 
     fs::path(dest_dir_root,
              dest_folder ,
              stringr::str_glue("atfm_slot_adherence_{YYYY}.csv", YYYY = .y$YEAR)),
-    na = ""))
+    na = ""),
+    .keep = TRUE)
 
